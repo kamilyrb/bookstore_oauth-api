2d702e405bd54d5d2f3df4f5f620a578346b3284
Kamil Yarbaşı <kamilyrb@gmail.com>
2021-05-24 02:51:46 +0300

    feature/cassandra-integration - integrate cassandra connection - add check cassandra connection on StartApplication func

--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/kamilyrb/bookstore_oauth-api/src/clients/cassandra"
 	"github.com/kamilyrb/bookstore_oauth-api/src/domain/access_token"
 	"github.com/kamilyrb/bookstore_oauth-api/src/utils/errors"
 )
@@ -17,5 +18,11 @@ func NewRepository() DbRepository {
 }
 
 func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors.RestErr) {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	// TODO: implement get access token from CassandraDB
 	return nil, errors.NewInternalServerError("database connection not implemented yet")
 }
