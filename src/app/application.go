d5cc5eac7fa4c1a8c7faba27a6b7144eba7e42c1
Kamil Yarbaşı <kamilyrb@gmail.com>
2021-05-29 23:24:53 +0300

    feature/create-access-token - move service methods to new created services module - add crypto_utils - add AccessTokenRequest dao struct - expand NewService method of access_token service as can take dbRepository and restUsersRepository parameters

--- src/app/application.go
+++ src/app/application.go
@@ -2,9 +2,10 @@ package app
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/kamilyrb/bookstore_oauth-api/src/domain/access_token"
 	"github.com/kamilyrb/bookstore_oauth-api/src/http"
 	"github.com/kamilyrb/bookstore_oauth-api/src/repository/db"
+	"github.com/kamilyrb/bookstore_oauth-api/src/repository/rest"
+	"github.com/kamilyrb/bookstore_oauth-api/src/services/access_token"
 )
 
 var (
@@ -12,11 +13,12 @@ var (
 )
 
 func StartApplication() {
-	atService := access_token.NewService(db.NewRepository())
+
+	atService := access_token.NewService(rest.NewRepository(), db.NewRepository())
 	atHandler := http.NewHandler(atService)
 
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
 	router.POST("/oauth/access_token", atHandler.Create)
 
-	router.Run(":8080")
+	router.Run(":8001")
 }
--- src/domain/users/user.go
+++ src/domain/users/user.go
@@ -1,7 +1,7 @@
 package users
 
 type User struct {
-	Id        int    `json:"id"`
+	Id        int64  `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
